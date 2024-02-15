36f9c371d2a77eb9e60747d9de96ce25dcf86323
AlekseiDeripasko <109969285+AlekseiDeripasko@users.noreply.github.com>
2024-02-15 19:45:04 +0200

    Add files via upload
    Я лох

---
+++ sprint09/oderypasko/t08/src/mx_atoi.c
@@ -0,0 +1,37 @@
+#include "minilibmx.h"
+
+bool mx_isdigit(char c);
+bool mx_isspace(char c);
+
+int mx_atoi(const char *str)
+{
+    int result = 0;
+    int sign = 1;
+
+    while (mx_isspace(*str))
+    {
+        str++;
+    }
+
+    if (*str == '-')
+    {
+        sign = -1;
+        str++;
+    }
+    else if (*str == '+')
+    {
+        str++;
+    }
+
+    while (mx_isdigit(*str))
+    {    
+        result *= 10;
+        result += *str - '0';
+        
+        str++;
+    }
+
+    return result * sign;
+}
+
+
